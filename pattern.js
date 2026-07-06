e883db9268677567aea7c1b8e5ef1a27da1c0ead
r52027002-spec <r52027002@gmail.com>
2026-07-07 08:02:55 +0900

    pattern.js を更新

--- pattern.js
+++ pattern.js
@@ -1,1 +1,39 @@
+import {
+    generatePipeCircle,
+    calculateIntersectionCurve
+} from "./geometry.js";
 
+
+export function createPattern(options) {
+
+    const data = {
+
+        mainDiameter: options.mainDiameter,
+        branchDiameter: options.branchDiameter,
+        angle: options.angle,
+        divisions: options.divisions ?? 180,
+
+        points: {
+            main: generatePipeCircle({
+                diameter: options.mainDiameter,
+                divisions: options.divisions ?? 180
+            }),
+
+            branch: generatePipeCircle({
+                diameter: options.branchDiameter,
+                divisions: options.divisions ?? 180
+            })
+        },
+
+        intersectionCurve:
+            calculateIntersectionCurve({
+                mainDiameter: options.mainDiameter,
+                branchDiameter: options.branchDiameter,
+                angle: options.angle,
+                divisions: options.divisions ?? 180
+            })
+    };
+
+
+    return data;
+}
